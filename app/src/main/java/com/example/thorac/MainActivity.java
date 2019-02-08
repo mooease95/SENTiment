8dd8976f47842773c8f9e4b824cb973db6d08e3e
ma2019 <ILuvPK4816X!>
2019-02-08 15:38:02 +0000

    - Fixed appcompat

--- app/src/main/java/com/example/thorac/MainActivity.java
+++ app/src/main/java/com/example/thorac/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.thorac;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
